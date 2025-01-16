c2a8a170700aba6e3153a3b9ec1e2dccdeedb416
sunil kudipudi <89185025+sunil32773@users.noreply.github.com>
2025-01-16 11:13:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ mongoose
     console.log("Connected to MongoDB");
   })
   .catch((err) => console.log(err));
+  app.get("/",(req,res)=>{
+    res.send("<h1>Hello World</h1>");
+  })
 
 app.listen(port, () => {
   console.log("Server running on port 5000");
